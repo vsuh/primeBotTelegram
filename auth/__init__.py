a90dab748905bdd6405e8cc31015231adc71e0a1
vsuh <vsuh@mail.ru>
2021-03-11 08:41:53 +0300

    Перенес токен и чатид в переменные окpужения (heroku)

--- auth/__init__.py
+++
@@ -1,4 +0,0 @@
-TOKEN = '358010524:AAGslQzG7xCrmm_-gfFhX9QXHnwc-tLLPks' # VsuhMCBot
-CHTID = '-572989583'                                    # hi456 channel
-TGAPI = 'https://api.telegram.com'                                            
-MYURL = ''
